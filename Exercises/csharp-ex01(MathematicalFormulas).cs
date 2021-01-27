05380541b8674fa9c162702cf1c1fed3d96644f3
Brandon Scott <brandon.e.scott@outlook.com>
2021-01-27 12:05:56 -0500

    first exercise commit

---
+++ Exercises/csharp-ex01(MathematicalFormulas).cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathematicalFormulas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nPart 1: circumference and area of a circle.");
+            Console.Write("Enter an integer for the radius: ");
+            string strradius = Console.ReadLine();
+            int intradius = int.Parse(strradius);
+            double circumference = 2 * Math.PI * intradius;
+            double area = Math.PI * (intradius * intradius);
+            Console.WriteLine($"The circumference is {circumference}");
+            Console.WriteLine($"The area is {area}");
+
+            Console.WriteLine("\nPart 2: volume of a hemisphere.");
+            double volume = (4 / 3 * Math.PI * (intradius * intradius * intradius)) / 2;
+            Console.WriteLine($"The volume is {volume}");
+
+            Console.WriteLine("\nPart 3: area of a triangle (Heron's Formula).");
+            Console.Write("Enter an integer for side a: ");
+            string strsidea = Console.ReadLine();
+            int intsidea = int.Parse(strsidea);
+            Console.Write("Enter an integer for side b: ");
+            string strsideb = Console.ReadLine();
+            int intsideb = int.Parse(strsideb);
+            Console.Write("Enter an integer for side c: ");
+            string strsidec = Console.ReadLine();
+            int intsidec = int.Parse(strsidec);
+            double p = (intsidea + intsideb + intsidec) / 2;
+            area = Math.Sqrt(p * (p - intsidea) * (p - intsideb) * (p - intsidec));
+            Console.WriteLine($"The area is {area}");
+
+            Console.WriteLine("\nPart 4: solving a quadratic equation.");
+            Console.Write("Enter an integer for coefficient a: ");
+            string stra = Console.ReadLine();
+            Console.Write("Enter an integer for coefficient b: ");
+            string strb = Console.ReadLine();
+            Console.Write("Enter an integer for coefficient c: ");
+            string strc = Console.ReadLine();
+            double dbla = double.Parse(stra);
+            double dblb = double.Parse(strb);
+            double dblc = double.Parse(strc);
+            double sqrtpart = (dblb * dblb) - (4 * dbla * dblc);
+            double denominator = 2 * dbla;
+            if (sqrtpart > 0)
+            {
+                double positive_num = (-dblb + Math.Sqrt(sqrtpart));
+                double negative_num = (- dblb - Math.Sqrt(sqrtpart));
+                Console.WriteLine($"The positive solution is {positive_num / denominator}.");
+                Console.WriteLine($"The negative solution is {negative_num / denominator}.");
+            }
+            else if (sqrtpart < 0)
+            {
+                Console.WriteLine("Cannot take the square root of a negative number.");
+            }
+        }
+    }
+}
---
+++ Exercises/csharp-ex02(RecurssiveMethods).cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursiveMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Part 1 (sum of 10 numbers)
+            Console.WriteLine("Part 1, sum 10 numbers.");
+            int sum = SumTenInts(0, 1);
+            char letterGrade = 'X';
+            Console.WriteLine($"The sum is {sum}");
+            
+            // Part 2 (avg 10 scores)
+            Console.WriteLine("Part 2, average 10 numbers.");
+            double avg = AvgTenInts(0, 1);
+            letterGrade = ConvertNumericToLetterGrade(avg);
+            Console.WriteLine($"The average of ten integers is {avg} and the letter grade is {letterGrade}");
+
+            // Part 3, average user predetermined number of scores
+            Console.WriteLine("Part 3, average user predetermined number of scores.");
+            Console.Write("How many scores do you wish to enter? ");
+            string noScores = Console.ReadLine();
+            int numScores = int.Parse(noScores);
+            double avg1 = AvgUnkInts(0, 1, numScores);
+            letterGrade = ConvertNumericToLetterGrade(avg1);
+            Console.WriteLine($"The average of {numScores} integers is {avg1} and the letter grade is {letterGrade}");
+
+            // Part 4, average non-predetermined number of scores
+            Console.WriteLine("Part 4, average non-predetermined number of scores.");
+            double avg2 = AvgAnyInts(0, 1);
+            letterGrade = ConvertNumericToLetterGrade(avg2); 
+            Console.WriteLine($"The average of the integers is {avg2} and the letter grade is {letterGrade}");
+        }
+
+        private static char ConvertNumericToLetterGrade(double grade)
+        {
+            if (grade >= 90)
+                return 'A';
+            else if (grade >= 80 && grade < 90)
+                return 'B';
+            else if (grade >= 70 && grade < 80)
+                return 'C';
+            else if (grade >= 60 && grade < 70)
+                return 'D';
+            else
+                return 'F';
+        }
+
+        private static int SumTenInts(int sum, int count)
+        {
+            Console.Write("Enter a score: ");
+            string input = Console.ReadLine();
+            sum += int.Parse(input);
+            if (count < 10)
+                return SumTenInts(sum, count + 1);
+            else
+                return sum;
+        }
+
+        private static double AvgTenInts(int sum, int count)
+        {
+            Console.Write("Enter a score: ");
+            string input = Console.ReadLine();
+            sum += int.Parse(input);
+            if (count < 10)
+                return AvgTenInts(sum, count + 1);
+            else
+                return sum / count;
+        }
+
+        private static double AvgUnkInts(int sum, int count, int numScores)
+        {
+            Console.Write("Enter a score: ");
+            string input = Console.ReadLine();
+            sum += int.Parse(input);
+            if (count < numScores)
+                return AvgUnkInts(sum, count + 1, numScores);
+            else
+                return sum / count;
+        }
+        private static double AvgAnyInts(int sum, int count)
+        {
+            Console.Write("Enter a score (enter Quit when complete): ");
+            string input = Console.ReadLine();
+            if (input == "Quit")
+                return sum / --count;
+            else
+                sum += int.Parse(input);
+            if (count > 0)
+                return AvgAnyInts(sum, count + 1);
+            else
+                return sum / --count;
+        }
+    }
+}
