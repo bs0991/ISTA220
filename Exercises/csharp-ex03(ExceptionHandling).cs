8f2763aa0c8254b4f8903153147d70d34308f0b7
Brandon Scott <brandon.e.scott@outlook.com>
2021-02-08 10:26:29 -0500

    updated exercises

---
+++ Exercises/csharp-ex03(ExceptionHandling).cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_ex03_ExceptionHandling_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is C Sharp exercise 03.");
+            exceptionHandling();
+        }
+
+        private static double exceptionHandling()
+        {
+            // Part 1
+            {
+                var circarea = circumferenceValue();
+                Console.WriteLine($"The circumference is {circarea.Item1}");
+                Console.WriteLine($"The area is {circarea.Item2}");
+            }
+
+            // Part 2
+            {
+                double volume = volumeValue();
+                Console.WriteLine($"The volume is {volume}");
+            }
+
+            // Part 3
+            {
+                double area = areaValue();
+                Console.WriteLine($"The area is {area}");
+            }
+
+            // Part 4
+            {
+                var quadratic = quadraticValue();
+                Console.WriteLine($"The positive solution is {quadratic.Item1}.");
+                Console.WriteLine($"The negative solution is {quadratic.Item2}.");
+            }
+            return 0;
+        }                
+
+        private static Tuple<double,double> circumferenceValue()
+        {
+            try
+            {
+                Console.WriteLine("Part 1: Circumference and area of a circle.");
+                Console.Write("Enter an integer for the radius: ");
+                string strradius = Console.ReadLine();
+                int intradius = int.Parse(strradius);
+                double circumference = 2 * Math.PI * intradius;
+                double area = Math.PI * (intradius * intradius);
+
+                var circarea = Tuple.Create(circumference, area);
+
+                if (intradius > 0)
+                {
+                    return circarea;
+                }
+
+                if (intradius < 0)
+                {
+                    throw new ArgumentException("Your number is out of range.");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("You must enter a valid number.");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Your number is out of range.");
+            }
+            finally
+            {
+                Console.WriteLine("Okay");
+            }
+            return circumferenceValue();            
+        }
+
+        private static double volumeValue()
+        {
+            try
+            {
+                Console.WriteLine("Part 2: Volume of a hemisphere.");
+                Console.Write("Enter an integer for the radius: ");
+                string strradius = Console.ReadLine();
+                int intradius = int.Parse(strradius);
+                double volume = (4 / 3 * Math.PI * (intradius * intradius * intradius)) / 2;
+
+                if (intradius > 0)
+                {
+                    return volume;
+                }
+
+                if (intradius < 0)
+                {
+                    throw new ArgumentException("Your number is out of range.");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("You must enter a valid number.");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Your number is out of range.");
+            }
+            finally
+            {
+                Console.WriteLine("Okay");
+            }
+            return volumeValue();
+        }
+
+        private static double areaValue()
+        {
+            try
+            {
+                Console.WriteLine("Part 3: area of a triangle (Heron's Formula).");
+
+                Console.Write("Enter an integer for side a: ");
+                string strsidea = Console.ReadLine();
+                int intsidea = int.Parse(strsidea);
+
+                Console.Write("Enter an integer for side b: ");
+                string strsideb = Console.ReadLine();
+                int intsideb = int.Parse(strsideb);
+
+                Console.Write("Enter an integer for side c: ");
+                string strsidec = Console.ReadLine();
+                int intsidec = int.Parse(strsidec);
+                double p = (intsidea + intsideb + intsidec) / 2;
+                double area = 0;
+                area = Math.Sqrt(p * (p - intsidea) * (p - intsideb) * (p - intsidec));
+
+                if (intsidea > 0)
+                {
+                    return intsidea;
+                }
+
+                if (intsidea < 0)
+                {
+                    throw new ArgumentException("Your number is out of range.");
+                }
+
+                if (intsideb > 0)
+                {
+                    return intsideb;
+                }
+
+                if (intsideb < 0)
+                {
+                    throw new ArgumentException("Your number is out of range.");
+                }
+
+                if (intsidec > 0)
+                {
+                    return intsidec;
+                }
+
+                if (intsidec < 0)
+                {
+                    throw new ArgumentException("Your number is out of range.");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("You must enter a valid number.");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Your number is out of range.");
+            }
+            finally
+            {
+                Console.WriteLine("Okay");
+            }
+            return areaValue();
+
+        }
+
+        private static Tuple<double, double> quadraticValue()
+        {
+            try
+            {
+                Console.WriteLine("\nPart 4: solving a quadratic equation.");
+
+                Console.Write("Enter an integer for coefficient a: ");
+                string stra = Console.ReadLine();
+
+                Console.Write("Enter an integer for coefficient b: ");
+                string strb = Console.ReadLine();
+
+                Console.Write("Enter an integer for coefficient c: ");
+                string strc = Console.ReadLine();
+
+                double dbla = double.Parse(stra);
+                double dblb = double.Parse(strb);
+                double dblc = double.Parse(strc);
+                double sqrtpart = (dblb * dblb) - (4 * dbla * dblc);
+                double denominator = 2 * dbla;
+
+                double positive_num = (-dblb + Math.Sqrt(sqrtpart));
+                double negative_num = (-dblb - Math.Sqrt(sqrtpart));
+
+                double result1 = positive_num / denominator;
+                double result2 = negative_num / denominator;
+
+                var quadratic = Tuple.Create(result1, result2);
+
+
+                if (sqrtpart < 0)
+                {
+                    Console.WriteLine("Cannot take the square root of a negative number.");
+                }
+                else
+                    return quadratic;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("You must enter a valid number.");
+            }
+            finally
+            {
+                Console.WriteLine("Okay");
+            }
+            return quadraticValue();
+        }
+    }
+}
+ 
+                
+   
---
+++ Exercises/csharp-ex04(AnimalFarm).cs
@@ -0,0 +1,240 @@
+﻿using System;
+
+namespace AnimalFarm
+{
+    public class Animal
+    {
+        public string Name;
+        public string Type;
+        public string Food;
+        public string Sound;
+
+        public void Hello()
+        {
+            Console.WriteLine($"Hi! My name is {Name} and I am a {Type}!");
+        }
+        public void Eat()
+        {
+            Console.Write($"I like to eat {Food} ");
+        }
+        public void Speak()
+        {
+            Console.WriteLine($"and I say {Sound}");
+        }
+    }
+    class Chicken : Animal
+    {
+        public Chicken()
+        {
+            Console.WriteLine("Welcome to my Animal Farm!");
+            Console.WriteLine("Allow me to introduce you to my favorite chickens!");
+        }
+
+        public Chicken(string name, string type, string food, string sound)
+        {
+            Name = name;
+            Type = type;
+            Food = food;
+            Sound = sound;
+        }
+    }
+
+    class Horse : Animal
+    {
+        public Horse()
+        {
+            Console.WriteLine("And next I'll introduce you to some of my favorite horsies!");
+        }
+
+        public Horse(string name, string type, string food, string sound)
+        {
+            Name = name;
+            Type = type;
+            Food = food;
+            Sound = sound;
+        }
+    }
+
+    class Pig : Animal
+    {
+        public Pig()
+        {
+            Console.WriteLine("And next I'll introduce you to some of my favorite piggies!");
+        }
+
+        public Pig(string name, string type, string food, string sound)
+        {
+            Name = name;
+            Type = type;
+            Food = food;
+            Sound = sound;
+        }
+    }
+
+    class Dog : Animal
+    {
+        public Dog()
+        {
+            Console.WriteLine("And finally I'll introduce you to some of my favorite doggos!");
+        }
+
+        public Dog(string name, string type, string food, string sound)
+        {
+            Name = name;
+            Type = type;
+            Food = food;
+            Sound = sound;
+        }
+    }
+
+    class Program
+    {
+        static void farmAnimals()
+        {
+            // Introduction
+            Chicken Introduction = new Chicken();
+            Console.ReadKey();
+
+            // Chicken Class
+
+            Chicken Foghorn = new Chicken("Foghorn", "manly rooster", "corn on the cob", "I say, I say what's the big idea. Don't stand there gawkin' son, speak up!");
+            Foghorn.Hello();
+            Console.ReadKey();
+            Foghorn.Eat();
+            Foghorn.Speak();
+            Console.ReadKey();
+
+            Chicken MissPrissy = new Chicken("Miss Prissy", "lovely spinster hen", "honeydew melons", "that I will find myself a rooster husband!");
+            MissPrissy.Hello();
+            Console.ReadKey();
+            MissPrissy.Eat();
+            MissPrissy.Speak();
+            Console.ReadKey();
+
+            Chicken LadyKluck = new Chicken("Lady Kluck", "Scottish chicken serving as the lady-in-waiting of Maid Marian", "leafy greens and fruits!", "absence makes the heart grow fonder.");
+            LadyKluck.Hello();
+            Console.ReadKey();
+            LadyKluck.Eat();
+            LadyKluck.Speak();
+            Console.ReadKey();
+
+            Chicken Ernie = new Chicken("Ernie", "giant chicken", "steak and drink beer", "that I hate Peter Griffin.");
+            Ernie.Hello();
+            Console.ReadKey();
+            Ernie.Eat();
+            Ernie.Speak();
+            Console.ReadKey();
+            Console.WriteLine();
+
+            // Horse Class
+            Horse Introduction1 = new Horse();
+            Console.ReadKey();
+
+            Horse Maximus = new Horse("Maximus", "palace horse", "only the finest apples and berries", "Flynn Rider is my best friend!");
+            Maximus.Hello();
+            Console.ReadKey();
+            Maximus.Eat();
+            Maximus.Speak();
+            Console.ReadKey();
+
+            Horse Spirit = new Horse("Spirit", "wild horse", "whatever I want, because I'm wild and free", "NEIGH!");
+            Spirit.Hello();
+            Console.ReadKey();
+            Spirit.Eat();
+            Spirit.Speak();
+            Console.ReadKey();
+
+            Horse Khan = new Horse("Khan", "horse of the Fa family", "ramen noodles", "that Mulan is my best friend!");
+            Khan.Hello();
+            Console.ReadKey();
+            Khan.Eat();
+            Khan.Speak();
+            Console.ReadKey();
+
+            Horse Pegasus = new Horse("Pegasus", "mythical divine horse", "Chick-fil-A", "that flying is the easiest way of travel.");
+            Pegasus.Hello();
+            Console.ReadKey();
+            Pegasus.Eat();
+            Pegasus.Speak();
+            Console.ReadKey();
+            Console.WriteLine();
+
+            // Pig Class
+            Pig Introduction2 = new Pig();
+            Console.ReadKey();
+
+            Pig Porky = new Pig("Porky","sylish pig with a bow-tie", "ribs", "th-th-th-that's all folks!");
+            Porky.Hello();
+            Console.ReadKey();
+            Porky.Eat();
+            Porky.Speak();
+            Console.ReadKey();
+
+            Pig MissPiggy = new Pig("Miss Piggy", "diva pig with attitude", "burgers", "never eat more than you can lift.");
+            MissPiggy.Hello();
+            Console.ReadKey();
+            MissPiggy.Eat();
+            MissPiggy.Speak();
+            Console.ReadKey();
+
+            Pig Piglet = new Pig("Piglet", "cute little pink piggie", "marshmellows", "if you have the courage to fail, you have the courage to succeed");
+            Piglet.Hello();
+            Console.ReadKey();
+            Piglet.Eat();
+            Piglet.Speak();
+            Console.ReadKey();
+
+            Pig Hamm = new Pig("Hamm", "plastic pig with a cork in my belly", "coins", "that Mr. Potato Head is my best friend!");
+            Hamm.Hello();
+            Console.ReadKey();
+            Hamm.Eat();
+            Hamm.Speak();
+            Console.ReadKey();
+            Console.WriteLine();
+
+            // Dog Class
+            Dog Introduction3 = new Dog();
+            Console.ReadKey();
+
+            Dog Snoopy = new Dog("Snoopy", "small pup with a happy dance", "peanuts", "the less you want the more you love.");
+            Snoopy.Hello();
+            Console.ReadKey();
+            Snoopy.Eat();
+            Snoopy.Speak();
+            Console.ReadKey();
+
+            Dog Droopy = new Dog("Droopy", "dog with a....you guessed it..a droopy face", "M&Ms", "don't worry be happy.");
+            Droopy.Hello();
+            Console.ReadKey();
+            Droopy.Eat();
+            Droopy.Speak();
+            Console.ReadKey();
+
+            Dog Odie = new Dog("Odie", "dog with yellow fur and brown ears", "cheetos", "that Garfield is my best friend!");
+            Odie.Hello();
+            Console.ReadKey();
+            Odie.Eat();
+            Odie.Speak();
+            Console.ReadKey();
+
+            Dog Pluto = new Dog("Pluto", "dog who just loves to play", "hotdogs", "that Micky is my best friend!");
+            Pluto.Hello();
+            Console.ReadKey();
+            Pluto.Eat();
+            Pluto.Speak();
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                farmAnimals();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
